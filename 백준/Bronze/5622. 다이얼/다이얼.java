5ea9e8bdbb6dc66e50c3e4f2ef4ba32d3ef75242
Gabee-ni <been.dev.kim@gmail.com>
2025-11-04 17:35:35 +0900

    [Bronze II] Title: 다이얼, Time: 164 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] str = sc.next().split("");
+		String[][] dial = {{null},{null},{"ABC"},{"DEF"},{"GHI"},{"JKL"},{"MNO"},{"PQRS"},{"TUV"},{"WXYZ"}};
+		int time = 0;
+		
+		for(int i=0; i<str.length; i++) {
+			for(int j=2; j<dial.length; j++) {
+				if(dial[j][0].contains(str[i])) {
+					time += (j+1);
+				}
+			}
+		}
+		System.out.println(time);
+		
+	}
+
+}
