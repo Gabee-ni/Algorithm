7a9a8129861a59b3081f8cdf114b84dec6560c3d
Gabee-ni <been.dev.kim@gmail.com>
2025-11-06 15:12:28 +0900

    [level 0] Title: 로그인 성공?, Time: 0.19 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         int cnt =0;
-        for(int k=0; k<id_pw.length;k++){
             System.out.println(id_pw[0]);
             for(int i=0; i<db.length ;i++){
                 //아이디가 동일한지 판별
@@ -12,7 +11,6 @@ class Solution {
                     }else return "wrong pw";
                 }
             }
-        }
         return "fail";
     }
 }
