b297ceeff40b23e84f6af2e0d86022d48b5845e5
Gabee-ni <been.dev.kim@gmail.com>
2025-11-10 08:52:47 +0900

    [level 0] Title: k의 개수, Time: 142.68 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        
+        for(int n=i; n<=j ;n++){
+            String[] str = (n+"").split("");
+            for(int m=0; m<str.length;m++){
+                 if(str[m].contains(k+""))answer++;
+           }           
+        }
+        
+        return answer;
+    }
+}
