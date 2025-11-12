3aba29619034240da3891256f04ba10e92981aa4
Gabee-ni <been.dev.kim@gmail.com>
2025-11-12 15:05:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    int y = sc.nextInt()-543;
+    System.out.println(y);
+
+    /*
+    default.java 파일이 존재하지 않을 경우 출력되는 기본 코드입니다.
+
+    기본 코드는 '기본 코드 설정' 버튼을 눌러 변경할 수 있습니다.
+    */
+  }
+}
