452789a78903ef058bc301f012bc5d8cf358a311
Gabee-ni <been.dev.kim@gmail.com>
2025-11-16 16:33:24 +0900

    [level 2] Title: 타겟 넘버, Time: 4.23 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,16 @@
 class Solution {
-public static int cnt=0;
     public int solution(int[] numbers, int target) {
-        int answer = dfs(0,0,0,numbers,target);
-        return answer;
+        
+        return dfs(0,0,numbers,target);
     }
     
-    public int dfs(int node, int num, int idx, int[] numbers, int target){
-        if(node==numbers.length){
-            if(num==target) return cnt ++;
-            else return cnt;
+    public int dfs(int idx, int sum,int[] numbers, int target){
+        if(idx==numbers.length){
+            if(sum==target) return 1;
+            else return 0;
         }
-        dfs(node+1, num+numbers[idx], idx+1, numbers, target);
-        dfs(node+1, num-numbers[idx], idx+1, numbers, target);
-        return cnt;
+        int plus = dfs(idx+1, sum+numbers[idx],numbers, target);
+        int minus = dfs(idx+1, sum-numbers[idx], numbers, target);
+        return plus+minus;
     }
 }
