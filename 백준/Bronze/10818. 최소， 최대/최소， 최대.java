de6d971977b4274d3ffb2d248880bbf80b868c5f
Gabee-ni <been.dev.kim@gmail.com>
2025-11-17 09:41:51 +0900

    [Bronze III] Title: 최소, 최대, Time: 1072 ms, Memory: 123940 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());  
+
+    int N = Integer.parseInt(st.nextToken());
+    
+    st = new StringTokenizer(br.readLine()); 
+    List<Integer> list = new ArrayList<>();
+
+    for(int i=0; i<N; i++){
+      list.add(Integer.parseInt(st.nextToken()));
+    }
+
+    Collections.sort(list);
+    System.out.println(list.get(0) +" "+ list.get(list.size()-1));    
+  }
+}
