9f75b6a356ba59bfd2e9772b2e52cd8d43f1ca5d
Gabee-ni <been.dev.kim@gmail.com>
2025-11-18 17:02:44 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());  
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int[] arr = new int[N];
+    
+    for(int i=0; i<N; i++){
+      arr[i] = i+1;
+    }
+
+    for(int i=0; i<M;i++){
+      st = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(st.nextToken())-1;
+      int end = Integer.parseInt(st.nextToken())-1;
+      
+      while(start <= end){
+        int temp = arr[end];
+        arr[end] = arr[start];
+        arr[start] = temp;
+
+        end--;
+        start++;
+      }
+    }
+
+    for(int num : arr){
+      System.out.print(num+" ");      
+    }
+
+    
+  }
+}
