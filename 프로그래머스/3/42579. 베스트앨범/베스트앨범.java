f31492d0a798ff026f0d8277e9c684e703d682f5
gabeen kim <been.dev.kim@gmail.com>
2025-11-24 18:15:15 +0900

    [level 3] Title: 베스트앨범, Time: 5.44 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,4 +1,5 @@
 import java.util.*;
+//고유번호, 재생횟수, 장르를 담는 song 객체 
 class Song{
     public int idx;
     public int play;
@@ -18,14 +19,19 @@ class Song{
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-   
+        //해당 장르가 몇개 있는지 카운팅 하려고 선언한 map
         Map<String, Integer> genreCount = new HashMap<>();
+        //장르 별 총 재생횟수를 카운팅하고 횟수 별로 내림차순 정렬하기 위한 map 
         Map<String, Integer> map = new HashMap<>();
+        //노래 객체를 담을 리스츠 
         List<Song> songList = new ArrayList<>();
         
         for(int i=0; i< genres.length; i++){
+            //장르별 노래 개수 카운팅 
             genreCount.put(genres[i], genreCount.getOrDefault(genres[i],0)+1);
+            //장르별 총 재생 횟수 카운팅
             map.put(genres[i], map.getOrDefault(genres[i],0)+plays[i]);
+            //노래 객체 리스트 
             songList.add(new Song(i,plays[i],genres[i]));
             
         }
@@ -40,8 +46,12 @@ class Solution {
             return map.get(o2).compareTo(map.get(o1));
         });
         
-        //idx 출력
+        //답을 담을 리스트 
         List<Integer> answer = new ArrayList<>();
+        //장르별로 노래를 2개씩 실을 수 있음.
+        //장르별로 순회하면서 노래가 해당 장르인지 판별하고, 
+        //만약 장르에 곡이 1개만 있을 경우는 바로 리스트에 담는다.
+        //1개가 아니라 2개 이상인 경우는 베스트 2곡만 담을 수 있도록 temp arr를 생성한 후 추후 list에 담아준다. 
         for(String genre : keySet){
             if(genreCount.get(genre)<2){
                 for(Song s : songList){
@@ -62,7 +72,7 @@ class Solution {
                     answer.add(num);
             }  
         }
-        
+        //답변을 arr 형식으로 반환해야 하므로 stream을 이용해 list -> array 변환 
         return answer.stream().mapToInt(i->i).toArray();
     }
 }
