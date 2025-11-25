50b07c4b41c8558bf7e6e38e15083a96627bce13
gabeen kim <been.dev.kim@gmail.com>
2025-11-26 08:44:55 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.05 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,39 +1,22 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = null;
-        int rowLen =0, colLen=0, sameLen=0;
-        if(arr1[0].length==arr2.length) {
-            sameLen = arr1[0].length;
-            rowLen = arr1.length;
-            colLen = arr2[0].length;
-            
-            answer = new int[rowLen][colLen];
-            for(int i=0; i< rowLen ; i++){
-                for(int j=0; j< colLen ; j++){
-                    int total = 0;
-                    for(int k=0;k<sameLen ; k++){
-                        total += (arr1[i][k] * arr2[k][j]);
-                    }
-                    answer[i][j] = total;
+
+        int rowLen =arr1.length;
+        int colLen= arr2[0].length; 
+        int sameLen = arr1[0].length;
+       
+        int[][] answer = new int[rowLen][colLen];
+        for(int i=0; i< rowLen ; i++){
+            for(int j=0; j< colLen ; j++){
+                int total = 0;
+                for(int k=0;k<sameLen ; k++){
+                    total += (arr1[i][k] * arr2[k][j]);
                 }
-            }
-        }else if(arr1.length==arr2[0].length){
-            sameLen = arr1.length;
-            rowLen = arr2.length;
-            colLen = arr1[0].length;
-            
-            answer = new int[rowLen][colLen];
-            for(int i=0; i< rowLen ; i++){
-                for(int j=0; j< colLen ; j++){
-                    int total = 0;
-                    for(int k=0;k<sameLen ; k++){
-                        total += (arr1[k][j] * arr2[i][k]);
-                    }
-                    answer[i][j] = total;
-                }
+                answer[i][j] = total;
             }
         }
+       
         return answer;
     }
 }
