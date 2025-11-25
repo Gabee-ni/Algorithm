9ce6e81f9784cb0b301172e1f877c9b3ec1ad408
gabeen kim <been.dev.kim@gmail.com>
2025-11-25 17:26:59 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -7,17 +7,16 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int[][] arr = new int[9][9];
 		int max =0; 
 		int row = 0, col = 0;
 		
-		for(int i=0; i<arr.length;i++) {
+		for(int i=0; i<9;i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			for(int j=0; j<arr.length;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(arr[i][j]>=max) {
-					max = arr[i][j];
+			for(int j=0; j<9;j++) {
+				int num = Integer.parseInt(st.nextToken());
+				if(num>=max) {
+					max = num;
 					row = i+1;
 					col = j+1;
 				}
