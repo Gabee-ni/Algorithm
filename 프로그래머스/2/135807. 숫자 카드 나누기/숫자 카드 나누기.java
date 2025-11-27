48dec927febfc730000dc4b8f3c70e05b5182275
gabeen kim <been.dev.kim@gmail.com>
2025-11-27 19:34:03 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 33.86 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    public int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+    
+    public boolean check(int[] arr, int num){
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]%num==0) return false; 
+        }
+        return true;
+    }
+
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        int numA = arrayA[0];
+        int numB = arrayB[0];
+        
+        for(int i=1; i<arrayA.length;i++){
+            numA = gcd(numA,arrayA[i]);
+        }  
+
+        for(int i=1; i<arrayB.length;i++){
+            numB = gcd(numB,arrayB[i]);
+        }
+
+        
+        //최대공약수나, 그 수의 약수들(최대공약수%i==0) 중에 조건을 만족하는 가장 큰 수 찾기 
+        for(int i=numA; i>1; i--){
+            if(numA%i==0 && check(arrayB,i)){
+                answer = Math.max(answer,i);
+                break;
+            }
+        }
+        for(int i=numB; i>=1; i--){
+            if(numB%i==0 && check(arrayA,i)){
+                answer = Math.max(answer,i);
+                break;
+            }
+        }
+        return answer;
+    }
+}
