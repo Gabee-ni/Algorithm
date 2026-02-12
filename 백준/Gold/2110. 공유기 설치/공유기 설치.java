e02797540c0458dbf1d717f5f6d0f13b23bac8a1
gabeen kim <been.dev.kim@gmail.com>
2026-02-12 17:30:01 +0900

    [Gold IV] Title: 공유기 설치, Time: 344 ms, Memory: 38696 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,56 +1,43 @@
-import java.util.*;
+import java.util.*; 
 import java.io.*;
-
-public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		int[] houses = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			houses[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(houses);
-		
-		int start = 0, end = 1000000000;
-		int answer = 0;
-		int mid =0; 
-		
-		while (start <= end) {
-			
-			mid = (start + end)/2;
-			int installCnt = 1;
-			int prev = houses[0];
-			
-//			System.out.println("start="+start + " end="+end + " mid="+mid);
-			for (int i=1; i<houses.length ; i++) {
-				if (houses[i] - prev >= mid) {
-					installCnt++;
-					prev = houses[i];
-				}
-//				System.out.println("i= "+ i +" installCnt="+installCnt + " prev="+prev);
-			}
-			
-			//설치개수 < C -> mid가 크니까 줄여야함. 
-			if (installCnt < C) {
-				end = mid-1; 
-			} else {
-				start = mid +1;
-				answer = mid;
-			}
-		
-		}
-	
-		System.out.println(answer);
-
-	}
-	
+class Main {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int[] home = new int[N];
+        
+        for (int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            home[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(home);
+        
+        int start =0, end =1000000000;
+        int answer = 0; 
+        
+        while (start <= end){
+            int mid = (start + end)/2;
+            int install = 1; //x1번 집 부터 설치
+            int prev = home[0];
+            
+            for (int i=1; i<home.length; i++){
+                if(home[i] - prev >= mid ){
+                    install++;
+                    prev = home[i];
+                }
+            } 
+            
+            if (install < C ){ 
+                end = mid-1;                
+            }  else {
+                answer = mid; 
+                start = mid+1;
+            }
+        }
+        System.out.println(answer);
+    }
 }
-
