8412baf4c2f0aceccfbadb14fe4fed1c1107e2ac
gabeen kim <been.dev.kim@gmail.com>
2026-02-19 18:23:10 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 52.91 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,57 +1,59 @@
 import java.util.*;
 
-
 class Solution {
+    static int id=1; 
     static int[] di = {-1,1,0,0};
     static int[] dj = {0,0,-1,1};
-    static int[][] visited;
-    static int id=1;
+    static int[][] visited; 
+    
     public int solution(int[][] land) {
         int answer = 0;
-        visited = new int[land.length][land[0].length];
         Map<Integer, Integer> map = new HashMap<>();
-
-        for (int j=0; j<land[0].length; j++) {
-            for (int i=0; i<land.length; i++) {
-                if (land[i][j] == 1 && visited[i][j]==0) {
-                    int totalSum = dfs( i, j, 0, land);
-                    map.put(id, totalSum);
+        visited = new int[land.length][land[0].length];
+        for(int j=0; j<land[0].length; j++){
+            for(int i=0; i<land.length; i++){
+                if(land[i][j] ==1 && visited[i][j] ==0){
+                    int size = dfs(j,i,0,land);
+                    map.put(id, size);
                     id++;
-                } 
-            }
-        }
-        
-        for (int j=0; j<land[0].length; j++) {
-            Set<Integer> set = new HashSet<>();
-            int totalSum=0;
-            for (int i=0; i<land.length; i++) {
-                if (visited[i][j] != 0 && !set.contains(visited[i][j])) {
-                totalSum += map.get(visited[i][j]);   
-                set.add(visited[i][j]);
                 }
             }
-            answer = Math.max(answer,totalSum);
         }
+        
+         for(int j=0; j<land[0].length; j++){
+            Set<Integer> set = new HashSet<>(); 
+            int totalSum= 0;
+            for(int i=0; i<land.length; i++){
+                if(land[i][j] == 1&& !set.contains(visited[i][j])){
+                    totalSum += map.get(visited[i][j]);
+                    set.add(visited[i][j]);
+                }
+            }
+             answer = Math.max(answer, totalSum);
+        }
+        
+        
         
         return answer;
-    } 
+    }
     
-    public int dfs(int i, int j, int cnt, int[][] land) {
-    
-        if ( visited[i][j]!= 0) return cnt;
+    public int dfs(int j, int i, int cnt, int[][] land){
+        if(visited[i][j] != 0) return cnt;
         
-        visited[i][j] = id;
+        visited[i][j] = id; 
         cnt+=1;
         
-        for(int k=0; k<4; k++) {
-            int ni = i + di[k];
-            int nj = j + dj[k];
+        for(int d=0; d<4; d++){
+            int ni = i + di[d];
+            int nj = j + dj[d];
             
-            if (ni<0 || nj<0 || ni>= land.length || nj >= land[0].length) continue;
-            if (land[ni][nj]==1 && visited[ni][nj]==0) {
-            cnt = dfs(ni, nj, cnt, land);
+            if(ni<0 || nj <0 || ni >= land.length || nj >= land[0].length ||visited[ni][nj]!=0) continue;
+            if(land[ni][nj] ==1) {
+                cnt = dfs(nj, ni, cnt, land);
             }
         }
+        
         return cnt;
+        
     }
 }
