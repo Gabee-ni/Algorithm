8c6371ed265fe90298a0cdf7f234dddac5c66422
gabeen kim <been.dev.kim@gmail.com>
2026-04-03 11:59:20 +0900

    [Gold IV] Title: 연구소, Time: 896 ms, Memory: 301324 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,93 +1,91 @@
-import java.util.*;
+import java.util.*; 
 import java.io.*; 
 
 class Main {
-    static class Location {
-        int li; 
-        int lj; 
-        
-        Location(int li, int lj){
-            this.li = li; 
-            this.lj = lj;
-        }
-    }
-    
-    static int[][] board; 
-    static int answer, N, M; 
-    static List<Location> list; 
-    static int[] di = {-1,1,0,0};
+    static int answer, N,M;
+    static int[][] map;
+    static int[] di ={-1,1,0,0};
     static int[] dj = {0,0,-1,1}; 
+    static  List<Loc> list;
+    static class Loc {
+        int i;
+        int j;
+        
+        Loc(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+       
+    }
     
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()); 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        list = new ArrayList<>();
         
-        N  = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        list = new ArrayList<>(); 
-        board = new int[N][M]; 
+        map = new int[N][M];
         
         for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for( int j=0; j<M;j++){
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j]==2) list.add(new Location(i,j));
+            st= new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken()); 
+                if(map[i][j]==2) list.add(new Loc(i,j)); 
             }
         }
         
         makeWall(0); 
-        System.out.println(answer);
+        System.out.println(answer); 
     }
-    static void makeWall(int count){
-        if(count==3){
-            
-            int[][] board2 = new int[N][M]; 
+    
+    static void makeWall(int wall){
+        if(wall == 3) {
+            //BFS
+            int[][] map2 = new int[N][M]; 
+            int count=0; 
             for(int i=0; i<N; i++){
-                board2[i] = board[i].clone();  
+                map2[i] = map[i].clone();
             }
             
-            Queue<Location> queue = new ArrayDeque<>(); 
-            for(Location l : list) {
+            Queue<Loc> queue = new ArrayDeque<>();
+            for(Loc l : list){
                 queue.offer(l); 
             }
             
             while(!queue.isEmpty()){
-                Location cur = queue.poll(); 
+                Loc cur = queue.poll(); 
                 
-                for(int d=0; d<4; d++){
-                    int ni = cur.li + di[d]; 
-                    int nj = cur.lj + dj[d]; 
+                for(int d=0; d<4;d++){
+                    int ni = cur.i + di[d];
+                    int nj = cur.j + dj[d]; 
                     
-                    if(ni <0 || nj <0 || ni >=N || nj >=M ) continue;
-                    if (board2[ni][nj] ==0) {
-                        board2[ni][nj] =2;
-                        queue.offer(new Location(ni, nj));
+                    if(ni<0||nj<0||ni>=N||nj>=M) continue;
+                    if(map2[ni][nj]==0) {
+                        map2[ni][nj] = 2;
+                        queue.offer(new Loc(ni, nj)); 
                     }
                 }
             }
             
-            int cnt =0; 
-         for(int i=0; i<N; i++){
-            for( int j=0; j<M;j++){
-                if(board2[i][j]==0) cnt++; 
+           for(int i =0 ; i<N; i++){
+            for(int j=0; j<M; j++){
+               if(map2[i][j] ==0 ) count++; 
             }
         }
-            answer = Math.max(cnt, answer); 
-            
-            return; 
+            answer = Math.max(answer, count); 
+            return;             
         }
         
-        for(int i=0; i<N; i++){
-            for( int j=0; j<M;j++){
-                if(board[i][j] == 0) {
-                    board[i][j] =1; 
-                    makeWall(count+1); 
-                    board[i][j] = 0; 
+        for(int i =0 ; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(map[i][j] == 0){
+                    map[i][j] = 1; 
+                    makeWall(wall+1);
+                    map[i][j] = 0; 
+       
                 }
-         
             }
         }
-        
-        
     }
 }
