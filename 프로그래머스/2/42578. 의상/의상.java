e0b12b6e2355b7ce695ad4b06210bb08cb3901e9
gabeen kim <been.dev.kim@gmail.com>
2026-05-05 09:11:20 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        
+        int answer = 1;
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<clothes.length; i++){
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
+        
+        for (int i=0; i<clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1); 
         }
-
-        int temp = 1;
-        for(String key : map.keySet()){
-            temp *= (map.get(key)+1);
+        for (Map.Entry<String, Integer> entry : map.entrySet()){
+            answer *= (entry.getValue()+1);
         }
-
-        if(map.size()>1) return temp-1;
-        else return clothes.length;
+        return answer-1;
     }
 }
