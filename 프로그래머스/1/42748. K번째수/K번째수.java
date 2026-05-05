396041f7dd4e243c5530767f6cf5e1168804bf35
gabeen kim <been.dev.kim@gmail.com>
2026-05-05 19:19:21 +0900

    [level 1] Title: K번째수, Time: 21.99 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i=0; i<commands.length; i++ ) {
             int start = commands[i][0] -1; 
             int end = commands[i][1] -1;
-            int idx = commands[i][2] -1; 
+            int idx = commands[i][2] -1;
             
             for (int j=start; j<=end; j++){
                 list.add(array[j]);
