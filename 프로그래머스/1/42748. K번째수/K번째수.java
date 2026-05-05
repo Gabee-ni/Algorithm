b7182c26b7ede6b7e17772cca0c13f1de7069e62
gabeen kim <been.dev.kim@gmail.com>
2026-05-05 19:11:36 +0900

    [level 1] Title: K번째수, Time: 3.43 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,17 +1,23 @@
-import java.util.*;
+import java.util.*; 
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        List<Integer> answer = new ArrayList<>(); 
+        List<Integer> list = new ArrayList<>();
         
-        for(int i=0; i< commands.length; i++){
-            List<Integer> list = new ArrayList<>();
-            for(int j=commands[i][0]-1; j< commands[i][1]; j++){
+        for (int i=0; i<commands.length; i++ ) {
+            int start = commands[i][0] -1; 
+            int end = commands[i][1] -1;
+            int idx = commands[i][2] -1; 
+            
+            for (int j=start; j<=end; j++){
                 list.add(array[j]);
             }
-            Collections.sort(list);
-            answer[i] = list.get(commands[i][2]-1);            
+            Collections.sort(list); 
+            answer.add(list.get(idx));
             
+            list.clear(); 
         }
-        return answer;
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
