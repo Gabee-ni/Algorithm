14aa37f8140dca033c53f16f015148827a130a85
gabeen kim <been.dev.kim@gmail.com>
2026-05-11 10:06:47 +0900

    [level 2] Title: 소수 찾기, Time: 6.85 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,37 +1,47 @@
 import java.util.*;
 class Solution {
-    
-    public boolean isPrime(int num) {
-        if(num < 2) return false;
-        for(int i=2; i<=(int)Math.sqrt(num); i++) {
-            if(num % i == 0) return false;
-        }        
-        return true;
-    }
-    
-    public void getPrime(int depth, String numbers, StringBuilder num, HashSet<String> set, boolean[] visited) {
-        if (depth == numbers.length()) return;
-        
-        for (int i=0; i< numbers.length(); i++) {
-            if(depth == 0 && numbers.charAt(i)=='0') continue;
-            if (visited[i]) continue;
-            
-            visited[i] = true;
-            num.append(String.valueOf(numbers.charAt(i)));
-            
-            if(isPrime(Integer.parseInt(num.toString()))) set.add(num.toString()); 
-            getPrime(depth+1, numbers, num, set, visited);
-            
-            visited[i] = false;
-            num.delete(num.length()-1, num.length());
-        }
-        return;
-    }
-    
+    Set<Integer> numberSet = new HashSet<>();
     
     public int solution(String numbers) {
-        HashSet<String> set = new HashSet<String>();
-        getPrime(0, numbers, new StringBuilder(), set, new boolean[numbers.length()]);  
-        return set.size();
+        String[] arr = numbers.split("");
+        boolean[] visited = new boolean[arr.length];
+        StringBuilder sb = new StringBuilder();
+         
+        dfs(arr, visited, sb);
+        
+        int answer = 0;
+        
+        for(int num : numberSet){
+            if(isPrime(num)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public void dfs (String[] arr, boolean[] visited, StringBuilder sb){
+        if(sb.length() > 0){
+            numberSet.add(Integer.parseInt(sb.toString()));
+        }
+        
+        for(int i=0; i<arr.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                sb.append(arr[i]);
+                
+                dfs(arr, visited, sb);
+                
+                sb.deleteCharAt(sb.length()-1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public boolean isPrime(int num){
+        if(num < 2) return false; 
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num %i == 0) return false;
+        }
+        return true;
     }
 }
