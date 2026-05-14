9deecc30052020046f176075854829f453c273db
gabeen kim <been.dev.kim@gmail.com>
2026-05-14 21:02:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 11.14 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,31 +1,24 @@
-import java.util.*;
+import java.util.*; 
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-        int time = 0;
-        int total = 0;
-        int idx = 0;
+        int answer = 0, idx = 0;
+        Deque<Integer> bridge = new ArrayDeque<>(); 
         
-        //현재 다리 길이만큼 0으로 초기화 
-        for (int i = 0; i < bridge_length; i++) queue.offer(0);
-
-        //시간은 항상 1초씩 증가해야 함. 
-        while (idx < truck_weights.length) {
-            time++;
-
-            total -= queue.poll();
-
-            if (total + truck_weights[idx] <= weight) {
-                queue.offer(truck_weights[idx]);
-                total += truck_weights[idx++];
-                
+        for (int i = 0; i<bridge_length; i++) bridge.offer(0);
+        int bridgeWeight = 0; 
+        
+        while(idx < truck_weights.length){
+            if (!bridge.isEmpty()) bridgeWeight -= bridge.poll();
+             
+            if (truck_weights[idx] + bridgeWeight <= weight){
+                bridge.offer(truck_weights[idx]); 
+                bridgeWeight += truck_weights[idx++]; 
             } else {
-                // 트럭 못 올리면 대기하는 만큼 0을 추가 => 이로써 대기 시간을 따로 카운팅 하지 않아도 됨 
-                queue.offer(0);
+                bridge.offer(0); 
             }
+            answer++; 
         }
-
-        // 마지막 트럭이 다리에서 빠져나가는 시간 추가
-        return time + bridge_length;
+        return answer + bridge_length;
     }
 }
