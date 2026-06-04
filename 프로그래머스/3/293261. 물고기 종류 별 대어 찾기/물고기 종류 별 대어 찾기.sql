deb2a53ef99d43b64bfacc26cf3318060469ee89
gabeen kim <been.dev.kim@gmail.com>
2026-06-04 20:51:00 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,14 +1,10 @@
-SELECT 
-    i.id,
-    t.fish_name,
-    t.length
-FROM fish_info i
+SELECT fi.id, tb.fish_name, tb.length 
+FROM fish_info fi 
 JOIN (
-    SELECT 
-        n.fish_type,
-        n.fish_name,
-        MAX(f.length) as length
-    FROM fish_info f
-    JOIN fish_name_info n ON f.fish_type = n.fish_type
-    GROUP BY fish_name, fish_type
-)t ON i.fish_type = t.fish_type AND i.length = t.length
+    SELECT i.fish_type, MAX(i.length) AS length, n.fish_name
+    FROM fish_info i
+    JOIN fish_name_info n ON i.fish_type = n.fish_type
+    WHERE length IS NOT NULL 
+    GROUP BY fish_type
+) tb ON fi.length = tb.length AND fi.fish_type = tb.fish_type
+
