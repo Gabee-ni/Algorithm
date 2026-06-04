aba7546a0040dbc54473853e78f70982248abcde
gabeen kim <been.dev.kim@gmail.com>
2026-06-04 21:33:18 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -1,9 +1,7 @@
 -- 코드를 입력하세요
-SELECT 
-    i.ingredient_type,
-    SUM(t.total_order) AS total_order
-FROM icecream_info i
-JOIN first_half t
-    ON i.flavor = t.flavor
+SELECT i.ingredient_type, SUM(total_order) AS total_order
+FROM first_half h 
+JOIN icecream_info i ON h.flavor = i.flavor 
 GROUP BY i.ingredient_type
-ORDER BY total_order 
+ORDER BY total_order
+    
